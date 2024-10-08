6a27b23b1017fc4ba61f0dec54a019c84ae5a903
AlexF43 <97276854+AlexF43@users.noreply.github.com>
2024-10-08 21:19:29 +1100

    Added Login Functionality

--- GymBuddy/LoginView.swift
+++ GymBuddy/LoginView.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    @State private var showAlert = false
+    @State private var alertMessage = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Email", text: $email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocapitalization(.none)
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Login") {
+                login()
+            }
+        }
+        .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                alertMessage = error.localizedDescription
+                showAlert = true
+            } else {
+                print("Login successful")
+            }
+        }
     }
 }
 
