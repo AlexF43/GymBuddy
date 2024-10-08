43e0f4c4e4d5252397bc030d58a0604499c9ffd4
AlexF43 <97276854+AlexF43@users.noreply.github.com>
2024-10-08 21:19:02 +1100

    Added Login Functionality

---
+++ GymBuddy/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  GymBuddy
+//
+//  Created by Alex Fogg on 8/10/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
